07a4d4d10014b9d91ed5ae65953aaa4bda1c88c7
darkcack <57454282+darkcack@users.noreply.github.com>
2020-09-10 01:00:04 +0300

    İş ilanı başvuru backend

--- kariyer/src/main/java/com/tez/kariyer/controller/JobAdvertiController.java
+++ kariyer/src/main/java/com/tez/kariyer/controller/JobAdvertiController.java
@@ -1,8 +1,16 @@
 package com.tez.kariyer.controller;
 
 import com.tez.kariyer.dto.JobAdvertiDTO;
+import com.tez.kariyer.dto.JobApplyDTO;
+import com.tez.kariyer.dto.JobPostDTO;
+import com.tez.kariyer.model.entity.JobApply;
 import com.tez.kariyer.model.entity.JobPosting;
+import com.tez.kariyer.model.entity.User;
+import com.tez.kariyer.model.repository.JobApplyRepository;
 import com.tez.kariyer.model.repository.JobPostingRepository;
+import com.tez.kariyer.response.ResponseItem;
+import com.tez.kariyer.security.SessionInfo;
+import com.tez.kariyer.service.JobApplyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +19,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +29,8 @@ public class JobAdvertiController {
 
     @Autowired
     JobPostingRepository jobPostingRepository;
+    @Autowired
+    JobApplyService jobApplyService;
 
     @GetMapping("/ilanlari")
     public ModelAndView show(Model model){
@@ -52,6 +63,25 @@ public class JobAdvertiController {
         model.addAttribute("date",dateFormat.format(job.getStartDate()));
         model.addAttribute("jobs",job);
         return modelAndView;
+    }
+
+
+    @PostMapping("/basvuru")
+    @ResponseBody
+    public ResponseItem saveJobPost(@RequestBody JobApplyDTO jobApplyDTO){
+        ResponseItem responseItem = new ResponseItem();
+
+        try {
+
+            responseItem= jobApplyService.saveJobApply(jobApplyDTO);
+
+            return responseItem;
+        }catch (Exception e){
+            responseItem.setResult(false);
+            responseItem.setMessage("İşlem Başarısız...");
+            e.printStackTrace();
+            return responseItem;
+        }
     }
 
 
---
+++ kariyer/src/main/java/com/tez/kariyer/model/repository/JobApplyRepository.java
@@ -0,0 +1,8 @@
+package com.tez.kariyer.model.repository;
+
+import com.tez.kariyer.model.entity.JobApply;
+import org.springframework.data.repository.CrudRepository;
+
+public interface JobApplyRepository extends CrudRepository<JobApply, Integer> {
+
+}
